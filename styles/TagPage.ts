deac571752406ab5100d5c6e8efa32b14ab0eb3b
Kenedy Lukito <kenedy.lukito@gmail.com>
2022-10-03 14:54:09 +0700

    style: fix market by tag title description spacing mobile view

--- styles/TagPage.ts
+++ styles/TagPage.ts
@@ -4,15 +4,15 @@ import tw from 'twin.macro';
 export const Description = styled.p`
   ${tw`font-sans text-gray-400 font-size[1rem]`}
   ${tw`lg:flex-1`}
-  ${tw`my-5`}
+  ${tw`my-5 px-5 lg:px-0`}
 `;
 
 export const TagDetail = styled.div`
-  ${tw`flex items-center`}
+  ${tw`flex items-center px-5 lg:px-0`}
 `;
 
 export const BackButton = styled.a`
   ${tw`font-medium text-decoration[underline] hover:text-decoration[none]`}
   ${tw`cursor-pointer`}
-  ${tw`block mb-5`}
+  ${tw`inline-block mb-5 mx-5 lg:mx-0`}
 `;
